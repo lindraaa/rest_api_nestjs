33a8e87d96f61d19e22b279fff43380a6626fd44
lindraaa <chancruz739@gmail.com>
2025-05-08 10:03:26 +0800

    edit db config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,24 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule,ConfigService } from "@nestjs/config";
 @Module({
     imports:[
-        ConfigModule.forRoot({
-            isGlobal: true,
-          }),
-        TypeOrmModule.forRoot({
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: (configService: ConfigService) => ({
             type: 'mysql',
-            host: 'localhost',
-            port: parseInt(process.env.DB_PORT as string),            
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
+            host: configService.get('DB_HOST'),
+            port: +configService.get('DB_PORT'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            database: configService.get('DB_NAME'),
             entities: [],
             synchronize: true,
-          }) 
+          }),
+          inject: [ConfigService],
+        }),
+
     ]
 
     
